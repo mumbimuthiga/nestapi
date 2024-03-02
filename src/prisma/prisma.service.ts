9a7f0ce584bf09a77d15c9a6b579d2642e1bff9f
agwenchez <agweraenock76@gmail.com>
2024-03-02 10:38:18 +0300

    feat: product management module with CRUD functionalities

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable({})
-export class PrismaService extends PrismaClient{
-    constructor(){
-        super({
-            datasources:{
-                db:{
-                    url:"postgresql://postgres:123@localhost:5432/nest?schema=public",
-                },
-            },
-        });
-    }
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123@localhost:5432/nest?schema=public',
+        },
+      },
+    });
+  }
 }
